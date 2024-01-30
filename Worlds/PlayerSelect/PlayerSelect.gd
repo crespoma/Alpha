cf03c103e319bf9b0d2300726ca2f15dff544815
crespoma <98053532+crespoma@users.noreply.github.com>
2024-01-29 20:13:30 -0500

    Working on first world after player select

--- Worlds/PlayerSelect/PlayerSelect.gd
+++ Worlds/PlayerSelect/PlayerSelect.gd
@@ -1,14 +1,37 @@
 extends Node2D
+@export var WizardStart: bool
+@export var ArcherStart: bool
+@export var KnightStart: bool
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_node("Knight/AnimatedSprite2D").play("Knight_Idle")
-	get_node("Archer/AnimatedSprite2D").play("Archer_Idle")
-	get_node("Wizard/AnimatedSprite2D").play("Wizard_Idle")
-	pass # Replace with function body.
-
-
+	get_node("Knight/CharacterBody2D/AnimatedSprite2D").play("Knight_Idle")
+	get_node("Archer/CharacterBody2D/AnimatedSprite2D").play("Archer_Idle")
+	get_node("Wizard/CharacterBody2D/AnimatedSprite2D").play("Wizard_Idle")
+	
+	
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+
+
+func _on_knight_start_pressed():
+	get_node("KnightStart").toggle_mode = not get_node("KnightStart").toggle_mode
+	get_tree().change_scene_to_file("res://Worlds/World1/World1.tscn")
+
+
+func _on_ranger_start_pressed():
+	
+	#get_node("ArcherStart").toggle_mode = not get_node("ArcherStart").toggle_mode
+	print(ArcherStart)
+	get_tree().change_scene_to_file("res://Worlds/World1/World1.tscn")
+
+
+func _on_wizard_start_pressed():
+	get_node("WizardStart").toggle_mode = not get_node("WizardStart").toggle_mode
+	get_tree().change_scene_to_file("res://Worlds/World1/World1.tscn")
+
+
---
+++ Worlds/World1/World1.gd
@@ -0,0 +1,20 @@
+extends "res://Worlds/PlayerSelect/PlayerSelect.gd" 
+
+var Player_Options = ["KnightStart","ArcherStart","WizardStart"]
+var Characters = ["Knight", "Archer", "Wizard"]
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print(ArcherStart)
+	
+
+	
+	
+
+	
+	
+	
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
